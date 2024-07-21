99d690ddeeea11cbf79c290336b11a97dbf30447
Ayush Amin <ayush.amin2022@vitstudent.ac.in>
2024-07-21 10:03:47 +0530

    model_trainer done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,8 +10,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
-# from src.components.model_trainer import ModelTrainerConfig
-# from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 # A dataclass is a class typically containing mainly data, although there aren't really any restrictions.
 @dataclass
@@ -63,4 +63,7 @@ if __name__=="__main__":
     train_data , test_data = obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr , test_arr, _ =  data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr , test_arr))
